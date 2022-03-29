ef7c91a78563ec731e33ffeb840318f2dc320e9c
leng <lengbuntith2000@gmail.com>
2022-03-29 23:41:00 +0700

    target spa client side

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,8 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  target: 'static',
+  ssr: false,
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '',
@@ -56,11 +58,4 @@ export default {
       },
     },
   },
-
-  serverMiddleware: [
-    {
-      path: 'api',
-      handler: '~/serverMiddleware/api',
-    },
-  ],
 }
