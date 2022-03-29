cd30520bd9e277098e3be691d66071cf58f01dba
Thorn324 <lychanthorn2000@gmail.com>
2022-03-29 23:02:45 +0700

    fixed

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,13 +20,11 @@ export default {
     css: [],
 
     // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-    plugins: [{ src: "~/plugins/vue2-google-maps.js" }],
+    plugins: [],
     /*
      ** Build configuration
      */
-    build: {
-        transpile: [/^vue2-google-maps($|\/)/],
-    },
+    build: {},
 
     // Auto import components: https://go.nuxtjs.dev/config-components
     components: true,
