873a49d1ee2bda26c9170b6a25da4f7066405b8a
Dalin Ourn <ourndalin111@gmail.com>
2022-05-16 15:16:48 +0700

    suggestion

---
+++ components/SuggestionCard.vue
@@ -0,0 +1,167 @@
+<template>
+  <div>
+    <v-tabs v-model="tabs" align-with-title>
+      <v-tab href="#all"> All </v-tab>
+      <v-tab href="#newest"> Newest </v-tab>
+      <v-tab href="#popular"> Popular </v-tab>
+      <v-tabs-slider color="yellow"></v-tabs-slider>
+    </v-tabs>
+
+    <v-row class="mt-10">
+      <v-col
+        v-for="suggestion in suggestions"
+        :key="suggestion._id"
+        height="300"
+        cols="12"
+        md="6"
+        lg="4"
+      >
+        <v-card outlined>
+          <div class="d-flex justify-center pa-2">
+            <div>
+              <v-img
+                gradient="to top right, rgba(0,0,0,.01), rgba(25,32,72,.7)"
+                class="white--text align-end"
+                width="320"
+                height="250"
+                :src="suggestion.image"
+              >
+                <div
+                  class="d-flex justify-center align-center"
+                  style="height: 250px; font-size: 20px; font-weight: 700"
+                >
+                  {{suggestion.title}}
+                </div>
+              </v-img>
+              <!-- <v-card-title class="d-flex justify-center">{{
+                suggestion.title
+              }}</v-card-title> -->
+              <hr />
+              <div class="mt-2 d-flex justify-center align-center">
+                <v-avatar>
+                  <v-img :src="suggestion.profile"> </v-img>
+                </v-avatar>
+
+                <div style="font-weight: 700" class="ml-2">
+                  {{ suggestion.name }}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <v-card-text height="140">
+            <div class="line-clamp">
+              {{ suggestion.des }}
+            </div>
+          </v-card-text>
+
+          <YourIdea />
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <!-- <div class="text-center">
+      <v-pagination v-model="page" :length="4" circle></v-pagination>
+    </div> -->
+<div class="text-center">
+    <v-pagination
+      v-model="page"
+      :length="10"
+      :total-visible="5"
+    ></v-pagination>
+  </div>
+  </div>
+  
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      page: 1,
+      suggestions: [
+        {
+          _id: 1,
+          image:
+            'https://images.unsplash.com/photo-1571844088753-73ca0880bcd9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YW5na29yJTIwd2F0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+          title: 'Angkor Thom',
+          des: ' Angkor Thom is home to some of the most popular temples and nearly all tours will incorporate parts of the great city.The Angkor Wat Small Circuit Tour will take you to Bayon, the Terrace of the Elephants, the Leper King Terrace, and many others...',
+          // profile: "../assets/pictures/pf1.png"
+          profile:
+            'https://www.pikpng.com/pngl/m/297-2978816_taehyung-transparent-circle-icons-jimin-png-taehyung-cute.png',
+          name: 'Vitou',
+        },
+
+        {
+          _id: 2,
+          image:
+            'https://miro.medium.com/max/1170/1*BZoLp2qFmu80KzncTFMnpQ.jpeg',
+          title: 'Floating Village',
+          des: ' I admire people who live in Floating Villages, travel to floating schools, and eat at Floating restaurants. It is very attractive for many visitors to Cambodia.',
+          profile:
+            'https://www.mockofun.com/wp-content/uploads/2019/12/circle-photo.jpg',
+          name: 'Tevy',
+        },
+        {
+          _id: 3,
+          image: 'https://ndm-travel.com/images/thumbnails/49448.png',
+          title: 'Tatai',
+          des: ' Amazing place. Really natural, and also the people there very friendly and the food reasonable price and the boat has fixed price. ',
+          name: 'Daro',
+          profile:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKXeLUu4iWlLdbXKgabEORQUq2v_w6mI_z5A&usqp=CAU',
+        },
+        {
+          _id: 4,
+          image:
+            'https://a0.muscache.com/im/pictures/69b16064-12b5-4763-b899-b52d55a452e7.jpg?im_w=720',
+          title: 'Koh Kong',
+          des: ' The island has many hills and is rich in natural scenery. There are waterfalls and you will stand mesmerized before the cascading waters of the fall from the height of 25 m.',
+          name: 'Neary',
+          profile:
+            'https://www.pikpng.com/pngl/b/355-3555383_circle-profile-picture-clipart.png',
+        },
+        {
+          _id: 5,
+          image:
+            'https://s9.kh1.co/32/32ec2412b401c8d64c42ca01d46869e0499a441c.gif',
+          title: 'Knong Psar',
+          des: " If you're a camping lover, Knong Psar is where we highly recommend you to explore in cambodia. The nature in there will let you to find something new.",
+          name: 'Yuthna',
+          profile:
+            'https://images.zapnito.com/cdn-cgi/image/metadata=copyright,fit=scale-down,format=auto,sharpen=1,quality=95/https://images.zapnito.com/users/557779/avatar/medium_I68rzl51SRu135F1WwKi_IMG_4370.JPG.png',
+        },
+        {
+          _id: 6,
+          image:
+            'https://img.theculturetrip.com/1440x807/smart/wp-content/uploads/2021/05/2c4j3en-e1625485939998.jpg',
+          title: 'Koh Ta Kiev',
+          des: ' Located in Ream National Park on the eastern edge of Sihanoukville, Koh Ta Kiev is just a stone’s throw from the mainland. ',
+          name: 'Khemrith',
+          profile:
+            'https://www.pinpng.com/pngs/m/629-6297014_purple-aesthetic-icon-city-lights-profile-pic-aesthetic.png',
+        },
+      ],
+      hasSaved: false,
+      model: null,
+    }
+  },
+
+  // // data () {
+  //   return {
+  //     page: 1
+  //   }
+  // // }
+  // page: 1
+}
+</script>
+
+<style>
+.line-clamp {
+  display: -webkit-box;
+  -webkit-line-clamp: 4;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+}
+</style>
---
+++ components/YourIdea.vue
@@ -0,0 +1,69 @@
+<template>
+    <div 
+                    class="pa-4 text-no-wrap rounded-pill d-flex justify-between align-center" 
+                    height="45">
+                        <v-text-field
+                        v-model="message"
+                        append-outer-icon="mdi-send"
+                        :prepend-icon="icon"
+                        filled
+                        clear-icon="mdi-close-circle"
+                        clearable
+                        label="Your idea ?"
+                        type="text"
+                        @click:append="toggleMarker"
+                        @click:append-outer="sendMessage"
+                        @click:prepend="changeIcon"
+                        @click:clear="clearMessage"
+                    ></v-text-field>
+                </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      password: 'Password',
+      show: false,
+      message: '',
+      marker: true,
+      iconIndex: 0,
+      icons: [
+        'mdi-emoticon',
+        'mdi-emoticon-cool',
+        'mdi-emoticon-dead',
+        'mdi-emoticon-excited',
+        'mdi-emoticon-happy',
+        'mdi-emoticon-neutral',
+        'mdi-emoticon-sad',
+        'mdi-emoticon-tongue',
+      ],
+    }),
+
+    computed: {
+      icon () {
+        return this.icons[this.iconIndex]
+      },
+    },
+
+    methods: {
+      toggleMarker () {
+        this.marker = !this.marker
+      },
+      sendMessage () {
+        this.resetIcon()
+        this.clearMessage()
+      },
+      clearMessage () {
+        this.message = ''
+      },
+      resetIcon () {
+        this.iconIndex = 0
+      },
+      changeIcon () {
+        this.iconIndex === this.icons.length - 1
+          ? this.iconIndex = 0
+          : this.iconIndex++
+      },
+    },
+  }
+</script>
