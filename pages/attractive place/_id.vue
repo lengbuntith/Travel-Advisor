ef960eb4fdb759f504f2adcb2657da00902c220a
piseth-kheng <pisethkheng8@gmail.com>
2022-05-31 14:42:25 +0700

    update cover attractive place

--- pages/attractive place/_id.vue
+++ pages/attractive place/_id.vue
@@ -1,17 +1,19 @@
 <template>
-  <BaseLayout>
+  <div>
     <AttractivePlaceCover />
-    <v-row class="mt-5">
-      <v-col cols="12" md="6">
-        <AttractivePlaceList />
-      </v-col>
-      <v-col cols="12" md="6">
-        <v-card outlined tile>
-          <AttractivePlaceLocation />
-        </v-card>
-      </v-col>
-    </v-row>
-  </BaseLayout>
+    <BaseLayout>
+      <v-row class="mt-5">
+        <v-col cols="12" md="6">
+          <AttractivePlaceList />
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-card outlined tile>
+            <AttractivePlaceLocation />
+          </v-card>
+        </v-col>
+      </v-row>
+    </BaseLayout>
+  </div>
 </template>
 
 <script>
